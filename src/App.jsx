d8da1d2da57dffe049dd50efeb3421b8b2ec3485
MohdRaza710 <mrmraza.710@gmail.com>
2025-12-04 12:22:54 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Navbar from "./Components/Navbar";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import PortFolio from "./Components/PortFolios";
-import Experiance from "./Components/Experiance";
-import Footer from "./Components/Footer";
-import Contact from "./Components/Contact";
-import Projects from "./Components/Projects";
+import Navbar from "./Pages/Navbar";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import PortFolio from "./Pages/PortFolios";
+import Experiance from "./Pages/Experiance";
+import Footer from "./Pages/Footer";
+import Contact from "./Pages/Contact";
+import Projects from "./Pages/Projects";
 import { Toaster } from "react-hot-toast";
 
 function App() {
